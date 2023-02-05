3b580d25d88521a0d301d34ea2cb69d31cebef89
shine10101 <pshine147@gmail.com>
2023-02-05 14:38:58 +0000

    populating table with value bets

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,12 +8,12 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+import pandas as pd
 
 
 @login_required(login_url="/login/")
 def index(request):
     context = {'segment': 'index'}
-
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
 
@@ -31,6 +31,9 @@ def pages(request):
             return HttpResponseRedirect(reverse('admin:index'))
         context['segment'] = load_template
 
+        data = dataframe_view(request)
+        context.update(data)
+
         html_template = loader.get_template('home/' + load_template)
         return HttpResponse(html_template.render(context, request))
 
@@ -42,3 +45,11 @@ def pages(request):
     except:
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
+
+def dataframe_view(request):
+    df = pd.read_csv(fr'C:\Users\Philip.Shine\PycharmProjects\valuebetting\predictions.csv')
+    df = df[['Date', 'Time', 'HomeTeam', 'AwayTeam', 'Max_Value', 'Max_Value_Result']]
+    df['Max_Value'] = (df['Max_Value']*100).round(1).astype(str)
+    df['Max_Value'] = df['Max_Value'] + '%'
+    data = df.to_dict(orient="records")
+    return {"data": data}
